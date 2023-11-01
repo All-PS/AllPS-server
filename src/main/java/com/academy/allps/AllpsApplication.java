d52d464e38649d7b7c9181a504955eb10e654b48
1Bronze <khdkkhdd@gmail.com>
2023-11-01 13:42:47 +0900

    init: create project

---
+++ src/main/java/com/academy/allps/AllpsApplication.java
@@ -0,0 +1,13 @@
+package com.academy.allps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllpsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllpsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/academy/allps/AllpsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.academy.allps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllpsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
