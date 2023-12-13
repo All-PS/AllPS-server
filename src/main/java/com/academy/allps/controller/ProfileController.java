cbc6f48a95f1c1deb8faebb59c13eb5c5a738579
1Bronze <khdkkhdd@gmail.com>
2023-12-14 01:06:48 +0900

    feat: implement Profile API for Blue-Green deployment

---
+++ src/main/java/com/academy/allps/controller/ProfileController.java
@@ -0,0 +1,26 @@
+package com.academy.allps.controller;
+
+import java.util.Arrays;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class ProfileController {
+    private final Environment env;
+
+    @GetMapping("/profile")
+    public String profile() {
+        List<String> profiles = Arrays.asList(env.getActiveProfiles());
+        List<String> serverProfiles = Arrays.asList("server1", "server2");
+        String defaultProfile = profiles.isEmpty() ? "default" : profiles.get(0);
+
+        return profiles.stream()
+                .filter(serverProfiles::contains)
+                .findAny()
+                .orElse(defaultProfile);
+    }
+}
