3e840e4e30eb60acc25691bbdd7e0a0f21f259e3
Kim Handong <khdkkhdd@gmail.com>
2023-12-15 18:03:32 +0900

    Update ProblemController.java

--- src/main/java/com/academy/allps/controller/ProblemController.java
+++ src/main/java/com/academy/allps/controller/ProblemController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@CrossOrigin(origins = "https://allps.io")
+@CrossOrigin(origins = "https://www.allps.io")
 public class ProblemController {
     private final ProblemService problemService;
 
